665cfc7efe03b2cab032bad94539751d939b3de9
Dejiwonder <princeadedejiadeyemi@gmail.com>
2025-08-26 22:27:39 +0200

    appp deploy

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print (1 +1 )
+
+import streamlit as st
+import pandas as pd
+
+# Load CSVs
+day365_df = pd.read_csv("C:/Users/Acer\Desktop/ML project/Wind_Power_Generation_Decision_APP/artifacts/day365_comparison.csv")
+day366_df = pd.read_csv("C:/Users/Acer\Desktop/ML project/Wind_Power_Generation_Decision_APP/artifacts/day366_forecast.csv")
+
+# Title
+st.title("🔋 Wind Power Forecast Dashboard")
+
+# Day 365 Comparison
+st.subheader("📊 Day 365: Actual vs Forecasted Power")
+st.dataframe(day365_df.style.format({
+    "Actual Power (MW)": "{:.2f}",
+    "Forecasted Power (MW)": "{:.2f}",
+    "Absolute Error": "{:.2f}",
+    "Error (%)": "{:.2f}"
+}))
+
+# Day 366 Forecast
+st.subheader("🔮 Day 366: Forecasted Power")
+st.dataframe(day366_df.style.format({
+    "Forecasted Power (MW)": "{:.2f}"
+}))
